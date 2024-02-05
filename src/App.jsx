a6464795256130e7e358d2697316d159161bb21c
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-02-05 14:05:05 +0545

    todo setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold font-sans text-red-500 drop-shadow-lg p-10">
+        Todo App
+      </h1>
+    </>
+  );
+}
+
+export default App;
